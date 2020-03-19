79289429d6d5060f2014edd93eee5e7d27e4aab5
yudai <yudai@student.ethz.ch>
2020-03-19 14:52:41 +0100

    Add Image Processing

--- ImgFeat.cpp
+++ ImgFeat.cpp
@@ -12,6 +12,11 @@ const char *keys =
     "{ help h |                  | Print help message. }"
     "{ input1 | box.png          | Path to input image 1. }"
     "{ input2 | box_in_scene.png | Path to input image 2. }";
+
+
+void feature_lab(){
+    
+}
 int main(int argc, char *argv[])
 {
     CommandLineParser parser(argc, argv, keys);
@@ -25,7 +30,7 @@ int main(int argc, char *argv[])
         return -1;
     }
     //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
-    int minHessian = 400;
+    int minHessian = 2000;
     Ptr<SURF> detector = SURF::create(minHessian);
     std::vector<KeyPoint> keypoints1, keypoints2;
     Mat descriptors1, descriptors2;
@@ -33,14 +38,29 @@ int main(int argc, char *argv[])
     detector->detectAndCompute(img2, noArray(), keypoints2, descriptors2);
     //-- Step 2: Matching descriptor vectors with a brute force matcher
     // Since SURF is a floating-point descriptor NORM_L2 is used
-    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::BRUTEFORCE);
-    std::vector<DMatch> matches;
-    matcher->match(descriptors1, descriptors2, matches);
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+    std::vector<std::vector<DMatch>> knn_matches;
+    matcher->knnMatch(descriptors1, descriptors2, knn_matches,2);
+
+    //define the threshold
+    const float threshold_const = 0.7;
+    std::vector<DMatch> in_matches;
+    //define the good and bad matches
+    for(int i=0;i<knn_matches.size();i++){
+       
+            if(knn_matches[i][0].distance<threshold_const*knn_matches[i][1].distance){
+                in_matches.push_back(knn_matches[i][0]);
+            }
+    }
     //-- Draw matches
-    Mat img_matches;
-    drawMatches(img1, keypoints1, img2, keypoints2, matches, img_matches);
+    Mat img_matches; // just stores the generated images
+    drawMatches(img1, keypoints1, img2, keypoints2, in_matches, img_matches, Scalar::all(-1), Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
     //-- Show detected matches
-    imshow("Matches", img_matches);
+    cout<<"descirptor size"<<descriptors1.size<<endl<<"desciptor2 size"<<descriptors2.size<<endl;
+    cout<<"matches's size"<<knn_matches.size();
+    cout<<"matches:"<<in_matches.size()<<endl;
+    namedWindow("Matches",WINDOW_NORMAL);
+    // imshow("Matches", img_matches);
     waitKey();
     return 0;
 }
